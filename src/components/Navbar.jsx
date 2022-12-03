a9942dc547e3cb7897da470f0e45b55e5f5ebdea
WildanJR09 <mwildjrs23@gmail.com>
2022-12-03 19:43:15 +0700

    Repair navbar indexs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     const handleClick = () => setNav(!nav)
 
     return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300 z-20'>
             <div>
                 <img src={Logo} alt="Logo" style={{ width: '250px' }} />
             </div>
